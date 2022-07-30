836877f7da2284847f186d8704c93ae8717876b6
Niall Barber <niall.barber@olioex.com>
2022-07-30 16:51:05 +0100

    Added flat list

---
+++ src/components/basic/FlatList/FlatList.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import {
+  FlatList as RnFlatList,
+  FlatListProps as RnFlatListProps,
+} from 'react-native'
+import { Factory } from '~@core/factory'
+
+const FlatList = <TItem extends RnFlatListProps<TItem>>({
+  data,
+  renderItem,
+  refs,
+  ...rest
+}: {
+  data: TItem[]
+  renderItem: JSX.Element | Array<JSX.Element>
+  refs?: any
+}) => {
+  const F = Factory(RnFlatList)
+
+  return (
+    <F
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={(item: any) => item.id}
+      ref={refs}
+      {...rest}
+    />
+  )
+}
+
+export default FlatList
