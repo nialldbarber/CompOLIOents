f3e31be33235e415eb1c095b09492d35ac8e0a40
Niall Barber <niall.barber@olioex.com>
2022-07-30 16:17:49 +0100

    Updated refs

--- src/components/primitives/Text/Text.tsx
+++ src/components/primitives/Text/Text.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Text as RnText } from '~@components/primitives/Text/styles'
+import { Factory } from '~@core/factory'
 import buttonVariants from '~@theme/variants/button'
 import sizeVariants, { Size } from '~@theme/variants/sizes'
 import { States } from '~@types/index'
@@ -17,13 +18,14 @@ const Text = ({
   size = 'medium',
   children,
 }: TextProps) => {
+  const T = Factory(RnText)
   const { fontSize } = sizeVariants[size]
   const { color } = buttonVariants[variants]
 
   return (
-    <RnText $variant={color} $fontSize={fontSize}>
+    <T $variant={color} $fontSize={fontSize}>
       {children}
-    </RnText>
+    </T>
   )
 }
 
